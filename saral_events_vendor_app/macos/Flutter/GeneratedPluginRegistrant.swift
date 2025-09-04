e86c97ca3046a41dc03addb8fbf82b1e3b449982
BHANU-SOLVINGCLUB <bhanu@solvingclub.org>
2025-09-04 13:33:40 +0530

    revert commit

--- saral_events_vendor_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ saral_events_vendor_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import device_info_plus
 import file_picker
 import file_selector_macos
 import google_sign_in_ios
-import package_info_plus
 import path_provider_foundation
 import pdfx
 import shared_preferences_foundation
@@ -23,7 +22,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
